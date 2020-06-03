98f82b643de0c5d717d1a2829f1bde74ef965567
Daniel Brandão <dcb@cin.ufpe.br>
2020-06-03 03:19:44 -0300

    Adding cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import cors from 'cors'
 import routes from './routes'
 import path from 'path'
 
 const app = express()
 
+app.use(cors({
+  origin: 'localhost'
+}))
 app.use(express.json())
 app.use(routes)
 
