414bfddd2939c3a97fc7b378c2681f27af32147d
Daniel Brandão <dcb@cin.ufpe.br>
2020-06-07 04:35:47 -0300

    Joi validation

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import multer from 'multer'
+import { celebrate, Joi } from 'celebrate'
 
 import multerConfig from './config/multer'
 
@@ -17,6 +18,22 @@ routes.get('/items', itemsController.index)
 
 routes.get('/points', pointsController.index)
 routes.get('/points/:id', pointsController.show)
-routes.post('/points', upload.single('image'), pointsController.create) // Todo: use multer.fileFilter to filter the  
+routes.post('/points', 
+  upload.single('image'), 
+  celebrate({
+    body: Joi.object().keys({                 // Todo: separate celebrate validation from routes
+      point: Joi.string().required()/*,
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      street: Joi.string().required(),
+      city: Joi.string().required(),
+      state: Joi.string().required().max(2),
+      items: Joi.string().required()*/
+    })
+  }, {abortEarly: false}), // Todo: Internationalization of the error messages
+  pointsController.create) // Todo: use multer.fileFilter to filter the  
 
 export default routes
